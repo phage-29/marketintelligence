1fa3e2a84d86cc2c3738bb418b45be7819aff7d7
Your Name <you@example.com>
2024-02-01 17:21:20 +0800

    up

--- msme.php
+++ msme.php
@@ -117,7 +117,7 @@ require_once "components/sidebar.php";
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">View Scorecard</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Scorecard</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
